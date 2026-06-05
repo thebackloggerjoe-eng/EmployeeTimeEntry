ae49063cccc010c9d2632937f86f8867858043ed
joefr <thebacklogger.joe@gmail.com>
2026-06-05 11:58:13 -0600

    getting to make this similar to a credit card application

--- JoeCardApplication/Controllers/HomeController.cs
+++ JoeCardApplication/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using JoeCardApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace JoeCardApplication.Controllers
 {
@@ -8,13 +9,28 @@ namespace JoeCardApplication.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public string APIKey = string.Empty;
+        // Host, marketingVersionCode, currentSite, apiSuccess, InputValidPass
+
+        public enum EventType { InvalidDataFormat = 2007, Unknown = 2000 };
+
+       // private readonly PageTrackingService _pageTracker;
+       // private readonly MemberAccessExceptionSettings _macSettings;
+       // private readonly IWebHostEnvironment _env;
+       // private readonly HostingEnvironmentInfo _envInfo;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            Console.WriteLine("right here");
         }
 
         public IActionResult Index()
         {
+            // CertificateRequest certificateRequest = new CertificateRequest();
+            // certificateRequest.marketingVersionCode = landingWelcomeSite(certificateRequest.currentSite);
+            // 
+
             return View();
         }
 
